c35ebae7220a1b5941647e76946ee475594ceaa7
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-01-01 14:51:13 +0000

    feat: simple pomodoro functionality added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,80 @@
+<script>
+	import { onDestroy } from 'svelte';
+
+	let timeLeft = 25 * 60;
+	let duration = 25 * 60;
+	let isRunning = false;
+	let isBreak = false;
+	let interval;
+
+	const POMODORO_TIME = 25 * 60;
+	const SHORT_BREAK_TIME = 5 * 60;
+	const LONG_BREAK_TIME = 15 * 60;
+	let pomodoroCount = 0;
+
+	function formatTime(seconds) {
+		const minutes = Math.floor(seconds / 60);
+		const secondsRemaining = seconds % 60;
+		return `${minutes.toString().padStart(2, '0')}:${secondsRemaining.toString().padStart(2, '0')}`;
+	}
+
+	function startTimer() {
+		if (!isRunning) {
+			isRunning = true;
+			interval = setInterval(() => {
+				if (timeLeft > 0) {
+					timeLeft--;
+				} else {
+					handleTimerComplete();
+				}
+			}, 1000);
+		}
+	}
+
+	function pauseTimer() {
+		if (isRunning) {
+			isRunning = false;
+			clearInterval(interval);
+		}
+	}
+
+	function resetTimer() {
+		pauseTimer();
+		timeLeft = isBreak ? SHORT_BREAK_TIME : POMODORO_TIME;
+		duration = timeLeft;
+	}
+
+	function handleTimerComplete() {
+		pauseTimer();
+		if (!isBreak) {
+			pomodoroCount++;
+			isBreak = true;
+			timeLeft = pomodoroCount % 4 === 0 ? LONG_BREAK_TIME : SHORT_BREAK_TIME;
+		} else {
+			isBreak = false;
+			timeLeft = POMODORO_TIME;
+		}
+		duration = timeLeft;
+		playNotification();
+	}
+
+	function playNotification() {
+		if (Notification.permission === 'granted') {
+			new Notification(isBreak ? 'Time to work' : 'Take a break');
+		}
+	}
+
+	function requestNotificationPermission() {
+		if ('Notification' in window && Notification.permission !== 'granted') {
+			Notification.requestPermission();
+		}
+	}
+
+	onDestroy(() => {
+		if (interval) clearInterval(interval);
+	});
+</script>
+
 <div class="navbar mb-8 bg-primary text-primary-content">
 	<div class="flex-1">
 		<btn class="btn btn-ghost text-xl">Pomodoist</btn>
@@ -17,10 +94,15 @@
 				<button class="btn btn-sm">Short Break</button>
 				<button class="btn btn-sm">Long Break</button>
 			</div>
-			<div class="my-8 text-center text-8xl font-bold">25:00</div>
-			<progress class="progress mx-auto my-4 w-80"></progress>
+			<div class="my-8 text-center text-8xl font-bold">{formatTime(timeLeft)}</div>
+			<progress class="progress mx-auto my-4 w-80" value={duration - timeLeft} max={duration}
+			></progress>
 			<div class="card-actions justify-center">
-				<button class="btn btn-primary">Start Timer</button>
+				{#if isRunning}
+					<button on:click={pauseTimer} class="btn btn-primary">Pause Timer</button>
+				{:else}
+					<button on:click={startTimer} class="btn btn-primary">Start Timer</button>
+				{/if}
 			</div>
 		</div>
 	</div>
