f41e722eb76a1357223ff325cd7437e18bf75a7b
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-01-02 22:45:02 +0000

    feat: task list sorting

--- src/lib/tasks.svelte
+++ src/lib/tasks.svelte
@@ -7,14 +7,53 @@
 	let tasksPerPage = $state(5);
 	let currentPage = $state(1);
 	let totalPages = $derived(Math.ceil(tasks.length / tasksPerPage));
-
+	let sortOption = $state('urgency-desc');
+	let sortOptions = [
+		{ value: 'urgency-desc', label: 'Urgency' },
+		{ value: 'manual-asc', label: 'Manual' },
+		{ value: 'content-asc', label: 'Name' },
+		{ value: 'due-asc', label: 'Due date' },
+		{ value: 'date-asc', label: 'Date added' },
+		{ value: 'prioriy-desc', label: 'Priority' }
+	];
 	let paginatedTasks = $derived.by(() => {
 		let filtered = [...tasks];
 		// apply filters
 		filtered = filtered.filter((task) => !task.isCompleted);
 
 		// sort
-		let sorted = filtered.sort((a, b) => b.urgency - a.urgency);
+		let sorted = [...filtered].sort((a, b) => {
+			const [field, direction] = sortOption.split('-');
+			let comparison;
+			switch (field) {
+				case 'content':
+					comparison = a.content.localeCompare(b.content);
+					break;
+				case 'manual':
+					comparison = a.order - b.order;
+					break;
+				case 'prioriy':
+					comparison = a.priority - b.priority;
+					break;
+				case 'date':
+					comparison = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+					break;
+				case 'due':
+					if (!a.due?.date && !b.due?.date) {
+						comparison = 0;
+					} else if (!a.due?.date) {
+						comparison = 1;
+					} else if (!b.due?.date) {
+						comparison = -1;
+					} else {
+						comparison = new Date(a.due.date).getTime() - new Date(b.due.date).getTime();
+					}
+					break;
+				default:
+					comparison = a.urgency - b.urgency;
+			}
+			return direction === 'desc' ? -comparison : comparison;
+		});
 
 		// apply pagination
 		const start = (currentPage - 1) * tasksPerPage;
@@ -22,7 +61,7 @@
 		return sorted.slice(start, end);
 	});
 
-	let projectDialog;
+	let projectDialog: HTMLDialogElement;
 
 	onMount(async () => {
 		tasks = await fetchTasks();
@@ -33,8 +72,13 @@
 	<div class="navbar">
 		<div class="flex-1 text-xl font-bold text-primary">Tasks</div>
 		<div class="flex-none">
-			<div onclick={() => projectDialog.showModal()} class="btn btn-ghost m-1">Projects</div>
+			<button onclick={() => projectDialog.showModal()} class="btn btn-ghost m-1">Projects</button>
 		</div>
+		<select bind:value={sortOption} class="select max-w-xs flex-none">
+			{#each sortOptions as option}
+				<option value={option.value} selected={option.value === sortOption}>{option.label}</option>
+			{/each}
+		</select>
 	</div>
 	{#each paginatedTasks as task}
 		<div id={task.id} class="alert my-4">
